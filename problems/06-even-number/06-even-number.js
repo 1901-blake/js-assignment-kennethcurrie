420282632839e824205b5ea2620803725772008b
kennethcurrie <kjcurrie@comcast.net>
2019-01-15 13:26:04 -0500

    Completed Even.js
    Created even checking code and TestCode

--- problems/06-even-number/06-even-number.js
+++ problems/06-even-number/06-even-number.js
@@ -3,5 +3,10 @@ Define function: isEven(someNum)
 Return true if even, false if odd.
 Do not use % operator. */
 function isEven(someNum) {
-  
+  return someNum/2 == Math.trunc(someNum/2);
+}
+
+//TestCode
+for (let index = 0; index < 11; index++) {
+    console.log(index+":\t"+isEven(index));   
 }
