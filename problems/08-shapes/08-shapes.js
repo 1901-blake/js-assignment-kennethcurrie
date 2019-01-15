f1f94984e803e4686706578e711a76e62688f0d9
kennethcurrie <kjcurrie@comcast.net>
2019-01-15 15:29:34 -0500

    completes shapes,js
    added shape generation TestCode

--- problems/08-shapes/08-shapes.js
+++ problems/08-shapes/08-shapes.js
@@ -21,5 +21,82 @@ Example for printShape("Diamond", 5, "*");
   * 
 */
 function printShape(shape, height, character) {
-  
+  shape = shape.toLowerCase();
+  if(isNaN(height)||height<1){
+    console.log("height must be a positive integer")
+    return NaN;
+  }
+  Math.trunc(height);
+  character = character.substring(0,1);
+  if(shape==="triangle"){
+    for (let x = 0; x < height; x++) {
+      let line = "";
+      for (let y = 0; y < x+1; y++){
+        line = line.concat(character);
+      }
+      console.log(line);
+    }
+  }
+  if(shape==="square"){
+    for (let x = 0; x < height; x++) {
+      let line = "";
+      for (let y = 0; y < height; y++){
+        line = line.concat(character);
+      }
+      console.log(line);
+    }
+  }
+  if(shape==="diamond"){
+    if(height%2===0){
+      for(x=0; x<height; x+=2){
+        let line ="";
+        for(y=0; y<x+2; y++){
+          line = line.concat(character);
+        }
+        console.log(pad(line,height));
+      }
+      for(x; x>0; x-=2){
+        let line ="";
+        for(y=0; y<x; y++){
+          line = line.concat(character);
+        }
+        console.log(pad(line,height));
+      }
+    }else{
+      for(x=-1; x<height; x+=2){
+        let line ="";
+        for(y=0; y<x+2; y++){
+          line = line.concat(character);
+        }
+        console.log(pad(line,height));
+      }
+      x-=2;
+      for(x; x>0; x-=2){
+        let line ="";
+        for(y=0; y<x; y++){
+          line = line.concat(character);
+        }
+        console.log(pad(line,height));
+      }
+    }
+  }
 }
+function pad(partial,length){
+  let sidepad = (length-partial.length)/2
+  if(sidepad<0){
+    return partial;
+  }
+  for (let index = 0; index < sidepad; index++) {
+    partial = " " + partial + " ";
+  }
+  return partial;
+}
+
+//TestCode
+printShape("Square",3,"%");
+console.log("------------------------------------");
+printShape("Triangle",3,"$");
+console.log("------------------------------------");
+printShape("Diamond",9,"*");
+console.log("------------------------------------");
+printShape("Diamond",8,"*");
