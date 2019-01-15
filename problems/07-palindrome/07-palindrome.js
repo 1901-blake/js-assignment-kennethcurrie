45486d77535d517c1e1ad360592cf93f0a78af0a
kennethcurrie <kjcurrie@comcast.net>
2019-01-15 13:54:44 -0500

    Completed palindrome.js
    Created Palindrome checking code and Testcode

--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -2,5 +2,20 @@
 Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
 function isPalindrome(someStr) {
-  
+    let midpoint = someStr.length/2;
+    let palindrome=true;
+    for (let index = 0; index < midpoint; index++) {
+        if(someStr.charAt(index)==someStr.charAt(someStr.length-(index+1))){
+            
+        }else{
+            return false;
+        }
+    }
+    return true
 }
+
+//TestCode
+console.log("abcba:\t" + isPalindrome("abcba"));
+console.log("abba:\t" + isPalindrome("abba"));
+console.log("abcdba:\t" + isPalindrome("abcdba"));
+console.log("abcddba:\t" + isPalindrome("abcddba"));
