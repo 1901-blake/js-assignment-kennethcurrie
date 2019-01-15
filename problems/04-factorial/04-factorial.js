ac19f01bebbb0a31c281b3ae845b358ef187d1d4
kennethcurrie <kjcurrie@comcast.net>
2019-01-15 11:27:35 -0500

    Created Factorial
    created Factorial code and test code

--- problems/04-factorial/04-factorial.js
+++ problems/04-factorial/04-factorial.js
@@ -2,5 +2,21 @@
 Define function: factorial(someNum)
 Use recursion to compute and return the factorial of someNum.  */
 function factorial(sumNum) {
-  
+  if(isNaN(sumNum)||sumNum<0){
+      return NaN;
+  }
+  sumNum = Math.trunc(sumNum);
+  if(sumNum===0||sumNum===1){
+      return 1;
+  }
+  let factValue = 1;
+  for (let index = 2; index < sumNum+1; index++) {
+      factValue*=index;
+  }
+  return factValue;
+}
+
+//TestCode
+for (let index = 0; index < 11; index++) {
+    console.log(factorial(index));
 }
