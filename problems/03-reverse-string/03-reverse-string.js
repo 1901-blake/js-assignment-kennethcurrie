e52ea67b5511434bae3bcff1686764d568a9c32f
kennethcurrie <kjcurrie@comcast.net>
2019-01-15 11:15:17 -0500

    Completed Reverse String
    Added string reversal code and test code

--- problems/03-reverse-string/03-reverse-string.js
+++ problems/03-reverse-string/03-reverse-string.js
@@ -2,5 +2,18 @@
 Define function: reverseStr(someStr)
 Reverse and return the String. */
 function reverseStr(someStr) {
-  
+  if(typeof(someStr)===typeof("TestString")){
+      let newString = someStr.charAt(someStr.length-1);
+      for (let index = someStr.length-2; index > -1; index--) {
+          newString=newString.concat(someStr.charAt(index));
+      }
+      return newString;
+  }else{
+      console.log("Not a String.");
+      return someStr;
+  }
 }
+
+
+console.log(reverseStr("Hello World!"));
+console.log(reverseStr(100));
