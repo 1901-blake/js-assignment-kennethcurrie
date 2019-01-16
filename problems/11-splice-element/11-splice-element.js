a1643cb017c91f3cba41fd9a8a13f58ff3473cd4
kennethcurrie <kjcurrie@comcast.net>
2019-01-16 09:45:21 -0500

    Completed splice.js
    Added splice code and TestCode

--- problems/11-splice-element/11-splice-element.js
+++ problems/11-splice-element/11-splice-element.js
@@ -1,7 +1,36 @@
 /* 11. Splice Element
 Define function spliceElement(someArr, index)
 Splice the element at the provided index in the array.
+
+Assumtion of meaning for splice: remove that specific element
 */
 function spliceElement(someArr, index) {
-  
+  if (isNaN(index)) {
+    console.log("Index must be a number");
+    return someArr;
+  }
+  if (index>someArr.length-1){
+    console.log("Index must be contained within array");
+    return someArr;
+  }
+  index = Math.trunc(index);
+  if (someArr.constructor === Array) {
+    let newArray = [] ;
+    someArr.forEach((element, I) => {
+       if (I===index){
+         console.log("Spliced out "+someArr[index]);
+       }else{
+         newArray.push(element);
+       }
+    });
+    return newArray;
+  }else{
+    console.log("Array must be provided to splice");
+    return someArr;
+  }
+  //replicate the splice function
+  //remove that item
 }
+
+//TestCode
+console.log(spliceElement([0,1,2,3,4,5,6,7,8,9],5));
