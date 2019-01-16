423b45c2ed0a67de55e6658768d9a025acf5867c
kennethcurrie <kjcurrie@comcast.net>
2019-01-16 09:58:28 -0500

    Created properties.js
    added property reading code and TestCode

--- problems/09-object-properties/09-object-properties.js
+++ problems/09-object-properties/09-object-properties.js
@@ -2,5 +2,12 @@
 Define function objectProperties(someObj)
 Print every property and it's value. */
 function objectProperties(someObj) {
-
+ //forin loop
+    for (let key in someObj) {
+        console.log(`${key}: ${someObj[key]}`);
+    }
 }
+
+//TestCode
+let car = {make:"Mustang", model:"Boss", mpg:18};
+objectProperties(car);
